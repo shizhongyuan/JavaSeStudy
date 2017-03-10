f95cba9848b9bd8a1c0ddc02bc9c699f4594a8b9
shizhongyuan <1067164628@qq.com>
2017-03-10 22:55:34 +0800

    initxd

---
+++ src/main/java/cn/edu/gxcme/study06/Huose.java
@@ -0,0 +1,19 @@
+package cn.edu.gxcme.study06;
+
+/**
+ * Created by Administrator on 2017/3/10 0010.
+ */
+public class Huose
+{            // 房子类
+    public int height;
+    public int width;
+
+}
+class ConcreteHuose  extends Huose
+{
+    String abc;
+}
+class WoodHuose extends Huose
+{
+    int ns;
+}
